bd023142c500dbd228cea508bd8cc03101efa6fe
Guy Marriott <guy@chec.io>
2021-12-07 16:42:04 -0800

    Track platform and Rutter IDs in integration storage, and prevent duplicating products

---
+++ platformMap.ts
@@ -0,0 +1,9 @@
+// Declare a list of platforms as they relate to template "codes", as we will have multiple integration templates, one
+// for each platform Rutter supports
+export default {
+  shopify: {
+    platform: 'SHOPIFY',
+    label: 'Shopify',
+  },
+  // TODO add more
+};
